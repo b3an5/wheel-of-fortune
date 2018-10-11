c5f79b107b014b48e950680d89ea5c3e1ae70a60
Jamie Rushford <jar.rushford@gmail.com>
2018-10-11 17:10:15 -0600

    Create Player Test for current code

---
+++ test/Player-test.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Player = require('../js/Player.js');
+// const spies = require('chai-spies');
+// chai.use(spies);
+
+describe('Player', () => {
+
+  var player;
+
+  beforeEach(() => {
+    player = new Player('John');
+  });
+
+  it('should have a name', () => {
+    expect(player.name).to.equal('John');
+  });
+
+  it('should start with an empty wallet', () => {
+    expect(player.wallet).to.equal(0);
+  });
+
+});
